49ace51a54dfd980d6aeb57f47f22c381213f1cf
Eileen Xue <xue.eileen@gmail.com>
2021-09-13 23:13:09 +0000

    Adding assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🛑 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("✅ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("two", 2);
