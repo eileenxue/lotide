945f1387be014fb98058076aae592024978b885a
Eileen Xue <xue.eileen@gmail.com>
2021-09-16 01:19:38 +0000

    Solved finding the key through value of an object

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+// assertArraysEqual Function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// CHALLENGE
+const findKeyByValue = function(object, value) {
+  // console.log(Object.keys(object));
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+// Test Code
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse")); // return: sci_fi
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
