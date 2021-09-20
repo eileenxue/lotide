3bcaab478050fc20051f58ce409accdb7c8809cb
Eileen Xue <xue.eileen@gmail.com>
2021-09-20 18:01:00 +0000

    Refactor tail.js

--- tail.js
+++ tail.js
@@ -1,35 +1,10 @@
 // Assert Equal Function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 // Tail Function
 const tail = function(array) {
   return array.slice(1);
 };
 
+module.exports = tail;
 
-// Word Array
-const words = ["Hello", "Lighthouse", "Labs"];
-
-// Test Case 1: Check the returned array elements
-const result = tail(words);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: Check to see that original array is not modified
-assertEqual(words.length, 3);
-
-
-// One element array
-const singleArray = ["Hello"];
-
-// Test cases
-const resultSingle = tail(singleArray);
-assertEqual(resultSingle.length, 0); // we should get back 0 elements
-assertEqual(resultSingle[0], undefined); // should be empty array or undefined?
